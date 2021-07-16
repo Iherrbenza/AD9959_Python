874acafd661a4ea706edb70e686c57d0f2b05904
Ivan Herrera Benzaquen <ibherrera@gmail.com>
2021-07-16 15:32:13 +1000

    Arduino files

---
+++ DDS_ESP32_Arduino/DDS_ESP32/Webserver.h
@@ -0,0 +1,27 @@
+
+// WiFi variables
+const char* ssid = "";
+const char* password = "";
+
+
+// Set web server port number to 80
+WiFiServer server(80);
+
+void connect_wifi(){ 
+  // Connect to Wi-Fi network with SSID and password
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {  
+    Serial.print("Trying to connect to ");
+    Serial.println(ssid);
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.macAddress());
+  server.begin();
+
+  }
